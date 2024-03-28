8e798d25d4a2c94785c897515bb7cd14baa2a334
TamNhien <canhthien91@gmail.com>
2024-03-28 20:09:03 +0700

    Add project files.

---
+++ P_ChecklistDangSong.py
@@ -0,0 +1,3 @@
+﻿L = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+print(all((L[i] > L[i - 1] and L[i] > L[i + 1]) or (L[i] < L[i - 1] and L[i] < L[i + 1]) for i in range(1, len(L) - 1)))
+
